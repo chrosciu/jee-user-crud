565be8bcef6ff2d90abd233bc282dc153d527b58
Marcin Chrost <marcinc@backbase.com>
2022-05-26 16:22:35 +0200

    working page for user list

---
+++ src/main/java/pl/coderslab/users/UserList.java
@@ -0,0 +1,14 @@
+package pl.coderslab.users;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/user/list")
+public class UserList extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/users/list.jsp")
+            .forward(request, response);
+    }
+}

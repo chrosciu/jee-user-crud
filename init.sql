f30f22201772504078223c13b4db9c8732c4717f
Marcin Chrost <mchrost82@gmail.com>
2022-12-16 17:22:44 +0100

    set id of user as primary key with auto generated value

--- init.sql
+++ init.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `users` (
-                         `id` int(11) NOT NULL,
+                         `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
                          `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
                          `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
                          `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO users (id, username, email, password) VALUES (1, 'chrosciu', 'mchrost82@gmail.com', NULL);
+INSERT INTO users (username, email, password) VALUES ('chrosciu', 'mchrost82@gmail.com', NULL);
